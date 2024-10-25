724b6a341e29ef99fca85e16ac22c95b4bb3ca34
quarkum-0 <122341624+quarkum-0@users.noreply.github.com>
2024-10-26 04:02:19 +0530

    must work

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://clothify-shop.vercel.app/api',
-        changeOrigin: true
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }    
   },
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
   }
-})
+});
